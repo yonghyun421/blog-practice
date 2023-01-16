24bba28015bed38b27c552d62272f1081a8e0840
yonghyun <dydgus0421@gmail.com>
2023-01-16 23:02:01 +0900

    Add: component 만들기

--- src/App.js
+++ src/App.js
@@ -49,8 +49,19 @@ function App() {
         <h4>{title[2]}</h4>
         <p>{date[2]}</p>
       </div>
+      <Modal />
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 
 export default App;
